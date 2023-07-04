3076727193cde7a6546af3835b78f1dee3f5a7bf
warfiretruck <138607454+warfiretruck@users.noreply.github.com>
2023-07-04 22:56:27 +0700

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -15,22 +15,22 @@ class LightFanControlSkill(MycroftSkill):
     @intent_file_handler('turn.light.on.intent')
     def handle_turn_light_on(self, message):
         self.speak_dialog('light.on')
-        GPIO.output(17, GPIO.HIGH)
+        GPIO.output(27, GPIO.HIGH)
 
     @intent_file_handler('turn.light.off.intent')
     def handle_turn_light_off(self, message):
         self.speak_dialog('light.off')
-        GPIO.output(17, GPIO.LOW)
+        GPIO.output(27, GPIO.LOW)
 
     @intent_file_handler('turn.fan.on.intent')
     def handle_turn_fan_on(self, message):
         self.speak_dialog('fan.on')
-        GPIO.output(27, GPIO.HIGH)
+        GPIO.output(22, GPIO.HIGH)
 
     @intent_file_handler('turn.fan.off.intent')
     def handle_turn_fan_off(self, message):
         self.speak_dialog('fan.off')
-        GPIO.output(27, GPIO.LOW)
+        GPIO.output(22, GPIO.LOW)
 
 
 def create_skill():
